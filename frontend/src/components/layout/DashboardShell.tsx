324324bd24ae46c55d7c6d08e17360214e8ef73e
Imanol Rodriguez Mini <irodriguezmini@gmail.com>
2026-06-17 16:10:34 -0300

    fix: remove clients prop from DashboardShell (Sidebar simplified)

--- frontend/src/components/layout/DashboardShell.tsx
+++ frontend/src/components/layout/DashboardShell.tsx
@@ -1,16 +1,14 @@
 'use client'
 import { Sidebar } from './Sidebar'
-import type { Client } from '@/types/metrics'
 
 interface Props {
-  clients:  Client[]
   children: React.ReactNode
 }
 
-export function DashboardShell({ clients, children }: Props) {
+export function DashboardShell({ children }: Props) {
   return (
     <div className="flex min-h-screen flex-col lg:flex-row">
-      <Sidebar clients={clients} />
+      <Sidebar />
       <main className="flex-1 overflow-auto bg-background">
         {children}
       </main>
